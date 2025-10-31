46810e073a9c2ebe09900cbbfea33b8229a9bcbe
therealxoulomon <thexoulomon@gmail.com>
2025-10-31 11:08:18 +0000

    chore: testing more bitcoindpc methods

--- btcrpc/src/main.rs
+++ btcrpc/src/main.rs
@@ -1,4 +1,5 @@
 use bitcoincore_rpc::{Auth, Client, RpcApi};
+use bitcoin::BlockHash;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create a connection to the local Bitcoin node
@@ -8,10 +9,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )?;
 
     // Fetch basic blockchain info
-    let info = rpc.get_blockchain_info()?;
-    println!("Chain: {}", info.chain);
-    println!("Block height: {}", info.blocks);
-    println!("Difficulty: {}", info.difficulty);
+    // let info = rpc.get_blockchain_info()?;
+    // println!("Chain: {}", info.chain);
+    // println!("Block height: {}", info.blocks);
+    // println!("Difficulty: {}", info.difficulty);
+
+        // Get wallet balance
+    // let balance = rpc.get_balance(None, None)?;
+    // println!("Wallet balance: {:.8} BTC", balance.to_btc());
+
+    // Get the best block hash
+    // let best_hash: BlockHash = rpc.get_best_block_hash()?;
+    // println!("Best block hash: {}", best_hash);
+
+    // // Fetch block details
+    // let block = rpc.get_block(&best_hash)?;
+    // println!("Block contains {} transactions", block.txdata.len());
+
+    // Network Info
+    let network_info = rpc.get_network_info()?;
+    println!("Version: {}", network_info.version);
+    println!("Connections: {}", network_info.connections);
+    // println!("Relay Fee: {:?}", network_info.relayfee);
+
 
     Ok(())
 }
